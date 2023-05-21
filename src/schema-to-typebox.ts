af00119bcdbeb7cf25bb70d18d4bb8e381f60038
Pierre Dahmani <hi@pierre-dev.com>
2023-05-21 22:59:52 +0200

    first case with JSON schema object and string property working
    added simple case of having a JSON schema with an object that contains a
    single given string works. also created a passing test 🎉

--- src/schema-to-typebox.ts
+++ src/schema-to-typebox.ts
@@ -7,7 +7,139 @@ export const schema2Typebox = (jsonSchema: string) => {
   // Otherwise I guess simply parsing the jsonSchema string into an object (I
   // guess it is always a JSON object?) and iterating the keys via something
   // like "Object.keys" should do it.
-  console.log("** for now we are just printing the schema itself **");
-  console.log(jsonSchema);
-  return jsonSchema;
+  // console.log("** for now we are just printing the schema itself **");
+  // console.log(jsonSchema);
+  const schemaObj = JSON.parse(jsonSchema);
+  console.error("Schema that was given\n", jsonSchema);
+
+  const typeBoxType = collect(schemaObj, []);
+  // TODO: rather use "title" from json schema than default to "T"
+  const valueName = "T";
+  const typeForObj = generateTypeForName(valueName);
+  return `import { Type, Static } from "@sinclair/typebox";
+
+${typeForObj}\nconst ${valueName} = ${typeBoxType}`;
 };
+
+const generateTypeForName = (name: string) => {
+  const [head, ...tail] = name;
+  if (head === undefined) {
+    throw new Error(`Can't generate type for empty string. Got input: ${name}`);
+  }
+  if (tail.length === 0) {
+    return `type ${head.toUpperCase()} = Static<typeof ${name}>`;
+  }
+  return `type ${head.toUpperCase}${tail.join("")} = Static<typeof ${name}>`;
+};
+
+/**
+ *
+ * @param requiredAttributes The required attributes/properties of the given schema object. Recursively passed down for each given object.
+ * @param propertyName The name of the attribute/property currently being collected.
+ * @throws Error
+ */
+export const collect = (
+  schemaObj: Record<string, any>,
+  requiredAttributes: string[] = [],
+  propertyName?: string
+): string => {
+  const type = getType(schemaObj);
+  if (type === "object") {
+    console.log("type was object");
+    const propertiesOfObj = getProperties(schemaObj);
+    // TODO: replace "as string[]" here
+    const requiredAttributesOfObject = (schemaObj["required"] ??
+      []) as string[];
+    const typeboxForProperties = propertiesOfObj.map(
+      ([propertyName, property]) => {
+        return collect(property, requiredAttributesOfObject, propertyName);
+      }
+    );
+    return `Type.Object({\n${typeboxForProperties}\n})`;
+  } else if (type === "string") {
+    if (propertyName === undefined) {
+      throw new Error("expected propertyName to be defined. Got: undefined");
+    }
+    return requiredAttributes.includes(propertyName)
+      ? `${propertyName}: Type.String()\n`
+      : `${propertyName}: Type.Optional(Type.String())\n`;
+  }
+  throw new Error(`cant collect ${type} yet`);
+};
+
+/**
+ * List of valid JSON schema values for the "type" attribute.
+ * src: https://datatracker.ietf.org/doc/html/draft-wright-json-schema-01#section-4.2
+ * src: https://json-schema.org/learn/miscellaneous-examples.html
+ *
+ * "An instance has one of six primitive types, and a range of possible
+   values depending on the type:"
+ */
+const VALID_TYPE_VALUES = ["object", "string"] as const;
+type VALID_TYPE_VALUE = (typeof VALID_TYPE_VALUES)[number];
+
+type PropertyName = string;
+type PropertiesOfProperty = Record<string, any>;
+
+/**
+ * Returns a list containing the name of the property and its properties (as
+ * object) for every property in the given schema.
+ *
+ * Example:
+ *
+ * ```
+ * {"properties":
+ *     { "name": {
+ *         "type": "string"
+ *       }
+ *     },
+ * }
+ * ```
+ *
+ * Returns
+ *
+ * ```
+ * [["name", {"type": "string"}]]
+ * ```
+ */
+const getProperties = (
+  schema: Record<string, any>
+): (readonly [PropertyName, PropertiesOfProperty])[] => {
+  const properties = schema["properties"];
+  if (properties === undefined) {
+    throw new Error(
+      "JSON schema was expected to have 'properties' attribute/property. Got: undefined"
+    );
+  }
+  const propertyNames = Object.keys(properties);
+  const listWithPropertyObjects = propertyNames.map((currItem) => {
+    if (currItem === undefined) {
+      throw new Error(`Value for property name ${currItem} was undefined.`);
+    }
+    const objectForProperty = properties[currItem] as Record<string, any>;
+    return [currItem, objectForProperty] as const;
+  });
+  return listWithPropertyObjects;
+};
+
+/**
+ * Gets the "type" of a JSON schema Instance
+ * src: https://datatracker.ietf.org/doc/html/draft-wright-json-schema-01#section-4.2
+ *
+ * @throws Error
+ */
+const getType = (schemaObj: Record<string, any>): VALID_TYPE_VALUE => {
+  const type = schemaObj["type"];
+
+  if (!VALID_TYPE_VALUES.includes(type)) {
+    throw new Error(
+      `JSON schema had invalid value for 'type' attribute. Got: ${type}`
+    );
+  }
+
+  return type;
+};
+
+export const generate = () => {};
+
+export const visit = () => {};
--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,79 @@
-import { describe, it } from "node:test";
+import { describe, test } from "node:test";
 import assert from "node:assert/strict";
+import * as prettier from "prettier";
+import {
+  schema2Typebox as Schema2Typebox,
+  collect,
+} from "../src/schema-to-typebox";
 
-describe("tests in seperate folder", () => {
-  it("works", () => {
-    assert.equal(true, true);
+const formatWithPrettier = (input: string): string => {
+  return prettier.format(input, { parser: "typescript" });
+};
+
+/**
+ * Formats given input with prettier and returns the result. This is used for
+ * testing to be able to compare generated types with expected types without
+ * having to take care of formatting.
+ * @throws Error
+ **/
+export const expectEqualIgnoreFormatting = (
+  input1: string,
+  input2: string
+): void => {
+  assert.equal(formatWithPrettier(input1), formatWithPrettier(input2));
+};
+
+describe("schema2typebox", () => {
+  test("object with required string property", () => {
+    const dummySchema = `
+    {
+      "type": "object",
+      "properties": {
+        "name": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "name"
+      ]
+    }`;
+    const expectedTypebox = `
+    import { Type, Static } from "@sinclair/typebox";
+
+    type T = Static<typeof T>;
+    const T = Type.Object({
+      name: Type.String(),
+    });
+    `;
+    expectEqualIgnoreFormatting(Schema2Typebox(dummySchema), expectedTypebox);
+  });
+  // TODO: probably rather test the collect() function than the schema2typebox
+  // one?
+});
+describe("schema2typebox - collect()", () => {
+  // NOTE: I currently think it is best to test the collect() function
+  // directly(less overhead) instead of schema2typebox for now.
+  test("object with required string property", () => {
+    const dummySchema = `
+    {
+      "type": "object",
+      "properties": {
+        "name": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "name"
+      ]
+    }`;
+    const expectedTypebox = `
+    Type.Object({
+      name: Type.String(),
+    });
+    `;
+    expectEqualIgnoreFormatting(
+      collect(JSON.parse(dummySchema)),
+      expectedTypebox
+    );
   });
 });
