de45ce11dc486ee9b089cece3e3e9bcdbfc6bd89
Pierre Dahmani <hi@pierre-dev.com>
2023-05-21 18:36:37 +0200

    remove unused. add comments

--- src/hello.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This is just a dummy file to show the "tests and code side by side" approach.
- */
-export const returnHelloWorld = (): "hello world" => {
-  return "hello world";
-};
--- src/schema-to-typebox.ts
+++ src/schema-to-typebox.ts
@@ -1,5 +1,12 @@
 /** Generates TypeBox code from JSON schema */
 export const schema2Typebox = (jsonSchema: string) => {
+  // Just thinking out "loud" with quick thoughts, not having put to much time
+  // into this.
+  //
+  // TODO: (perhaps?) check for any json schema parsers that are available.
+  // Otherwise I guess simply parsing the jsonSchema string into an object (I
+  // guess it is always a JSON object?) and iterating the keys via something
+  // like "Object.keys" should do it.
   console.log("** for now we are just printing the schema itself **");
   console.log(jsonSchema);
   return jsonSchema;
