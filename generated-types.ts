98677079dcbb833e444841f00f52832182f42e48
Pierre Dahmani <hi@pierre-dev.com>
2023-05-23 21:36:14 +0200

    fix generated-types

--- generated-types.ts
+++ generated-types.ts
@@ -1,6 +1,7 @@
 import { Type, Static } from "@sinclair/typebox";
 
-type T = Static<typeof T>
+type T = Static<typeof T>;
 const T = Type.Object({
-Type.Union([Type.Literal(1),Type.Literal(2)]),Type.Union([Type.String(),Type.Number(),Type.Null()])
-})
+  a: Type.Union([Type.Literal(1), Type.Literal(2)]),
+  b: Type.Union([Type.String(), Type.Number(), Type.Null()]),
+});
