7b6d7c7b8f1868f43969e46565b24a95a03e8929
Pierre Dahmani <hi@pierre-dev.com>
2023-05-23 21:40:13 +0200

    Feature/add any of (#7)
    add JSON schema `anyOf` to TypeBox `Type.Union()` implementation

--- generated-types.ts
+++ generated-types.ts
@@ -2,13 +2,6 @@ import { Type, Static } from "@sinclair/typebox";
 
 type T = Static<typeof T>;
 const T = Type.Object({
-  nickname: Type.Literal("xddq"),
-  x: Type.Literal(99),
-  y: Type.Literal(true),
-  z: Type.Literal(false),
-  a: Type.Array(Type.Literal(1)),
-  b: Type.Array(Type.Literal("hi")),
-  c: Type.Optional(Type.Literal(10)),
-  d: Type.Optional(Type.Array(Type.Literal(1))),
-  e: Type.Optional(Type.Array(Type.Literal("hi"))),
+  a: Type.Union([Type.Literal(1), Type.Literal(2)]),
+  b: Type.Union([Type.String(), Type.Number(), Type.Null()]),
 });
