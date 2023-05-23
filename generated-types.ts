dc70f933c3a00acd030f35d373926b693cd5f56f
Pierre Dahmani <hi@pierre-dev.com>
2023-05-23 23:32:03 +0200

    fix schemaOptions for type.Literal, fix schemaOptions for anyOf

--- generated-types.ts
+++ generated-types.ts
@@ -2,10 +2,9 @@ import { Type, Static } from "@sinclair/typebox";
 
 type T = Static<typeof T>;
 const T = Type.Object({
-  a: Type.Intersect([Type.Literal(1), Type.Literal(2)]),
-  b: Type.Optional(Type.Intersect([Type.String(), Type.Number()])),
-  c: Type.Intersect(
-    [Type.String({ description: "important" }), Type.Number({ minimum: 1 })],
-    { description: "intersection of two types" }
-  ),
+  a: Type.Union([Type.Literal(1), Type.Literal(2)]),
+  b: Type.Optional(Type.Union([Type.String(), Type.Number(), Type.Null()])),
+  c: Type.Union([Type.String({ maxLength: 20 }), Type.Literal(1)], {
+    description: "a union type",
+  }),
 });
