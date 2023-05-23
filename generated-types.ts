9b04233182b45435d5e9b07a7d7814fcf6ad4b61
Pierre Dahmani <hi@pierre-dev.com>
2023-05-23 22:50:14 +0200

    adds JSON schema allOf implementation which maps to Typebox.Intersect()

--- generated-types.ts
+++ generated-types.ts
@@ -2,6 +2,10 @@ import { Type, Static } from "@sinclair/typebox";
 
 type T = Static<typeof T>;
 const T = Type.Object({
-  a: Type.Union([Type.Literal(1), Type.Literal(2)]),
-  b: Type.Union([Type.String(), Type.Number(), Type.Null()]),
+  a: Type.Intersect([Type.Literal(1), Type.Literal(2)]),
+  b: Type.Optional(Type.Intersect([Type.String(), Type.Number()])),
+  c: Type.Intersect(
+    [Type.String({ description: "important" }), Type.Number({ minimum: 1 })],
+    { description: "intersection of two types" }
+  ),
 });
