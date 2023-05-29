ae0aca190cd77620f8f0d72632c51e7fbb59312e
Pierre Dahmani <hi@pierre-dev.com>
2023-05-29 15:16:11 +0200

    update eslintignore

--- test/index.ts
+++ test/index.ts
@@ -703,6 +703,11 @@ describe("schema2typebox internal - collect()", () => {
 // describe("cli usage", () => {
 // TODO: how can we test this?
 // test("pipes to stdout if -h or --help is given", async () => {
+//   Ideas:
+//     - can we provide process.stdout with our own stream and check the
+//   output?
+//     - can we mock process.stdout? does not work with mock.method since
+//     process.stdout is not a function.
 //   const getHelpTextMock = mock.method(getHelpText, "run", () => {});
 //   await runCli();
 //   assert.equal(getHelpTextMock.mock.callCount(), 10);
