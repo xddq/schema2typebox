c70dcd70c40165dc9c4bc8c6a6b4fe90c45919b4
Pierre Dahmani <hi@pierre-dev.com>
2023-06-27 21:46:08 +0200

    create example with ref to remote files

---
+++ examples/ref-to-remote-files/README.md
@@ -0,0 +1,10 @@
+# Example
+
+When schema2typebox is installed (simply issue npm i -g schema2typebox to do
+so), clone the repo, cd into this directory and:
+
+- Remove the old generated-typebox.ts file `rm generated-typebox.ts`
+- Use schema2typebox to generate the typebox code based on the schema.json file.
+  `schema2typebox`
+- generated-typebox.ts should now again exist in the folder
+- done 🎉
--- test/index.ts
+++ test/index.ts
@@ -310,10 +310,10 @@ describe("programmatic usage API", () => {
     const schemaWithRef = {
       anyOf: [
         {
-          $ref: "https://raw.githubusercontent.com/xddq/schema2typebox/feature/fix-refs-using-refparser/tmp/cat.json",
+          $ref: "https://raw.githubusercontent.com/xddq/schema2typebox/main/examples/with-ref-to-remote-files/cat.json",
         },
         {
-          $ref: "https://raw.githubusercontent.com/xddq/schema2typebox/feature/fix-refs-using-refparser/tmp/dog.json",
+          $ref: "https://raw.githubusercontent.com/xddq/schema2typebox/main/examples/with-ref-to-remote-files/dog.json",
         },
       ],
     };
