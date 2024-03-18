3ddb31c82315a43efd7bf740a32ae2a961c1ee43
matt stevens <52222076+croconut@users.noreply.github.com>
2024-03-18 12:13:03 -0700

    TS generics: 0, me: 1

---
+++ test/jsdoc_example.cjs
@@ -0,0 +1,27 @@
+// @ts-check
+const { TypeRegistry, Type, Kind } = require('@sinclair/typebox');
+const { Value } = require('@sinclair/typebox/value');
+
+TypeRegistry.Set('ExtendedOneOf', 
+  /** @type {(schema: any, value: unknown) => boolean} */
+  (schema, value) => {
+    return 1 === schema.oneOf.reduce(
+      /** @type {(acc: number, schema: any) => number} */
+      (acc, schema) => acc + (Value.Check(schema, value) ? 1 : 0), 
+    0);
+});
+
+/**
+ * @template {import("@sinclair/typebox").TSchema[]} T
+ * @param {[...T]} oneOf
+ * @param {import("@sinclair/typebox").SchemaOptions} options
+ * @returns {ReturnType<typeof Type.Unsafe<import("@sinclair/typebox").Static<import("@sinclair/typebox").TUnion<T>>>}
+ */
+function OneOf(oneOf, options = {}) {
+    // credit: https://github.com/microsoft/TypeScript/issues/27387#issuecomment-1223795056
+    return /** @type {typeof Type.Unsafe<import("@sinclair/typebox").Static<import("@sinclair/typebox").TUnion<T>>>} */ (Type.Unsafe)({ ...options, [Kind]: 'ExtendedOneOf', oneOf });
+};
+
+console.log(OneOf([Type.Object({"three": Type.Number()}), Type.Object({ "farquad": Type.String()})]));
+
+module.exports.OneOf = OneOf;
