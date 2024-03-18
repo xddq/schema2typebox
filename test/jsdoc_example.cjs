22c88875b0b1fe8763ae3e68caa5ddcb06838202
matt stevens <52222076+croconut@users.noreply.github.com>
2024-03-18 12:35:41 -0700

    lifted all possible types to defs, refined a few types

--- test/jsdoc_example.cjs
+++ test/jsdoc_example.cjs
@@ -2,19 +2,23 @@
 const { TypeRegistry, Type, Kind } = require('@sinclair/typebox');
 const { Value } = require('@sinclair/typebox/value');
 
+/**
+ * @typedef {import("@sinclair/typebox").TSchema} JSTSchema
+ * @typedef {import("@sinclair/typebox").SchemaOptions} JSSchemaOptions
+ */
 TypeRegistry.Set('ExtendedOneOf', 
-  /** @type {(schema: any, value: unknown) => boolean} */
+  /** @type {(schema: unknown & {oneOf: JSTSchema[]}, value: unknown) => boolean} */
   (schema, value) => {
     return 1 === schema.oneOf.reduce(
-      /** @type {(acc: number, schema: any) => number} */
+      /** @type {(acc: number, schema: JSTSchema) => number} */
       (acc, schema) => acc + (Value.Check(schema, value) ? 1 : 0), 
     0);
 });
 
 /**
- * @template {import("@sinclair/typebox").TSchema[]} T
+ * @template {JSTSchema[]} T
  * @param {[...T]} oneOf
- * @param {import("@sinclair/typebox").SchemaOptions} options
+ * @param {JSSchemaOptions} options
  * @returns {ReturnType<typeof Type.Unsafe<import("@sinclair/typebox").Static<import("@sinclair/typebox").TUnion<T>>>>}
  */
 function OneOf(oneOf, options = {}) {
